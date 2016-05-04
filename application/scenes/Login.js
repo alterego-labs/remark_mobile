74489d5e6760318d53db069935f33b28ff68f9d0
Sergey Gernyak <sergeg1990@gmail.com>
2016-05-04 15:08:13 +0300

    Basic view of login scene
    ref #1

--- application/scenes/Login.js
+++ application/scenes/Login.js
@@ -3,42 +3,92 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
 export default class Login extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.topContainer}>
+        <View style={ styles.logoContainer }>
+          <Text style={ styles.name }>REMARK</Text>
+        </View>
+        <View style={ styles.formContainer }>
+          <View style={ {backgroundColor: '#fff', marginLeft: 10, marginRight: 10, marginBottom: 10} }>
+            <TextInput
+              style={ styles.loginField }
+              placeholder="Enter your nickname"
+              placeholderTextColor="#bababa"
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <TouchableHighlight style={ styles.continueBtn }>
+            <Text style={ styles.continueBtnText }>Continue</Text>
+          </TouchableHighlight>
+        </View>
+        <Text style={ styles.copyright }>AlterEGO Labs, 2016</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  topContainer: {
+    backgroundColor: '#4589b0',
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'stretch',
+    flexDirection: 'column'
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  name: {
+    fontFamily: 'OpenSans-Semibold',
+    fontSize: 28,
+    letterSpacing: 3,
+    color: '#ffffff'
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  loginField: {
+    height: 60,
+    fontSize: 25,
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: '#000',
+    marginLeft: 10,
+    marginRight: 10,
+    fontFamily: 'OpenSans-Semibold'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  continueBtn: {
+    height: 60,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: '#1d628b',
+    backgroundColor: '#1d628b',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch'
   },
+  continueBtnText: {
+    color: '#fff',
+    fontSize: 25,
+    fontFamily: 'OpenSans-Semibold'
+  },
+  copyright: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontFamily: 'OpenSans-Semibold',
+    fontSize: 12
+  }
 });
