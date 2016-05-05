abb2481434445293ed0c6cfe0ad24cf0035efe5f
Sergey Gernyak <sergeg1990@gmail.com>
2016-05-05 22:03:48 +0300

    Added registering in GCM in main component

--- index.android.js
+++ index.android.js
@@ -1,11 +1,30 @@
 import React, {
   AppRegistry,
   Component,
+  DeviceEventEmitter
 } from 'react-native';
 
 import AppNavigator from './application/AppNavigator'
 
+var GcmAndroid = require('react-native-gcm-android');
+import Notification from 'react-native-system-notification';
+
 class RemarkMobile extends Component {
+  componentDidMount() {
+    GcmAndroid.addEventListener('register', (token) => {
+      console.log('send gcm token to server', token);
+    });
+    GcmAndroid.addEventListener('notification', (notification) => {
+      console.log('receive gcm notification', notification);
+    });
+
+    DeviceEventEmitter.addListener('sysNotificationClick', (e) => {
+      console.log('sysNotificationClick', e);
+    });
+
+    GcmAndroid.requestPermissions();
+  }
+
   render() {
     return (
       <AppNavigator />
