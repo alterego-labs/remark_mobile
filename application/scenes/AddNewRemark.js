4cb6bf4b1a62761119694ae4de3a6607df26f733
Sergey Gernyak <sergeg1990@gmail.com>
2016-05-29 20:28:40 +0300

    Render new remark form
    ref #7

--- application/scenes/AddNewRemark.js
+++ application/scenes/AddNewRemark.js
@@ -31,6 +31,21 @@ class AddNewRemark extends InsideLayout {
       </TouchableHighlight>
     );
   }
+
+  renderBody() {
+    return (
+      <View style={ styles.newRemarkForm }>
+        <Text style={ styles.formCaption }>New remark:</Text>
+        <TextInput
+          style={ styles.remarkBodyInput }
+          multiline={ true }
+        />
+        <TouchableHighlight style={ styles.saveBtn }>
+          <Text style={ styles.saveBtnText }>Save</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -43,7 +58,56 @@ const styles = StyleSheet.create({
     height: 25,
     width: 25,
     opacity: 0.5
+  },
+  formCaption: {
+    fontFamily: 'OpenSans-Semibold',
+    fontSize: 28,
+    letterSpacing: 3
+  },
+  newRemarkForm: {
+    marginLeft: 10,
+    marginRight: 10
+  },
+  remarkBodyInput: {
+    height: 200,
+    fontSize: 25,
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: '#000',
+    marginLeft: 10,
+    marginRight: 10,
+    fontFamily: 'OpenSans-Semibold'
+  },
+  saveBtn: {
+    height: 60,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: '#1d628b',
+    backgroundColor: '#1d628b',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch'
+  },
+  saveBtnText: {
+    color: '#fff',
+    fontSize: 25,
+    fontFamily: 'OpenSans-Semibold'
   }
 });
 
-export default AddNewRemark;
+AddNewRemark.propTypes = {
+  currentUser: React.PropTypes.object.isRequired
+}
+
+AddNewRemark.defaultProps = {
+  currentUser: { loggedIn: false, login: null, accessToken: null }
+}
+
+function mapStateToProps (state) {
+  return {
+    currentUser: state.auth.get('user')
+  };
+}
+
+export default connect(mapStateToProps)(AddNewRemark);
